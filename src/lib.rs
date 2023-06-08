876cd84dcabd0f9ab77dfc40337c0f3fe7ce9f92
mercuricchloride <mercuryprotocol@protonmail.com>
2023-06-08 13:22:11 -0700

    oneof types working

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod pb;
 
-use pb::soulbound_modules::v1::Foo;
+use pb::soulbound_modules::v1::{key_value, DynamicKeyValue, Foo, KeyValue};
+use prost::{encoding::message::encode, Message};
+use prost_types::{value::Kind, Value};
 use substreams::{self, errors::Error as SubstreamError};
 use substreams_ethereum::pb::eth::v2 as eth;
 
@@ -16,5 +18,24 @@ pub fn map_blocks(param: String, blk: eth::Block) -> Result<Foo, SubstreamError>
         })
     } else {
         Ok(Foo::default())
+    }
+}
+
+#[substreams::handlers::map]
+pub fn map_key_value(param: String, blk: eth::Block) -> Result<DynamicKeyValue, SubstreamError> {
+    if blk.number % 2 == 0 {
+        Ok(DynamicKeyValue {
+            keys: vec![KeyValue {
+                key: "foo".to_string(),
+                value: Some(key_value::Value::StringValue(param)),
+            }],
+        })
+    } else {
+        Ok(DynamicKeyValue {
+            keys: vec![KeyValue {
+                key: "bar".to_string(),
+                value: Some(key_value::Value::StringValue("asdflkjasdf".to_string())),
+            }],
+        })
     }
 }
