cf8a4eee37b4e84149620416e537a82c41c2dafb
mercuricchloride <mercuryprotocol@protonmail.com>
2023-07-20 09:14:50 -0700

    escaped run script, and added new endpoint

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ blur_address="0x000000000000Ad05Ccc4F10045630fb830B95127"
 blur_abi=$(cat ./blur_abi.json)
 milady_address="0x5Af0D9827E0c53E4799BB226655A1de152A425a5"
 
-map_events_input="${bayc_address}&&${bayc_abi}"
+map_events_input="${bayc_address}\&\&${bayc_abi}"
 filter_events_input="Transfer"
 filter_blur_trades_input=""
 
@@ -19,15 +19,18 @@ fi
 cp substreams.yaml substreams.yaml.bak
 
 # replace the placeholder with the input string
-sed -i "" "s|MAP_EVENTS_PARAMS|$map_events_input|g" substreams.yaml
-sed -i "" "s|FILTER_EVENTS_PARAMS|$filter_events_input|g" substreams.yaml
-sed -i "" "s|FILTER_BLUR_TRADES_PARAMS|$filter_blur_trades_input|g" substreams.yaml
-sed -i "" "s|ETHERSCAN_OVERVIEW_PARAMS|$map_events_input|g" substreams.yaml
+sed -i "s|MAP_EVENTS_PARAMS|$map_events_input|g" substreams.yaml
+sed -i "s|FILTER_EVENTS_PARAMS|$filter_events_input|g" substreams.yaml
+sed -i "s|FILTER_BLUR_TRADES_PARAMS|$filter_blur_trades_input|g" substreams.yaml
+sed -i "s|ETHERSCAN_OVERVIEW_PARAMS|$map_events_input|g" substreams.yaml
 
 # run the substream
-substreams run blur_trades \
+substreams run map_events \
 --start-block 17252197 \
---stop-block +200
+--stop-block +200 \
+--production-mode \
+-e eth.firehose.pinax.network:9000
+
 
 # restore the original file
 mv substreams.yaml.bak substreams.yaml
